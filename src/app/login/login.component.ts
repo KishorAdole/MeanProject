8a50cf5f34d2573edce8a5e2aafa1b6cb5ca3105
Kishor Adole <65148303+KishorAdole@users.noreply.github.com>
2020-09-05 02:38:03 +0530

    Updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faMapSigns } from '@fortawesome/free-solid-svg-icons';
 
 
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
@@ -15,33 +15,23 @@ export class LoginComponent implements OnInit {
   public faMapSigns = faMapSigns;
   public uiInvalidCredential = false;
 
-  public fbFormGroup = this.fb.group({
-    Email: ['', Validators.required],
-    Password: ['', Validators.required],
+  myFormGroup = new FormGroup({
+    "Email": new FormControl('', [
+      Validators.required, Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+[.]+[a-zA-z0-9-.]+$")
+    ]),
+    "Password": new FormControl()
   });
 
-  constructor(private fb: FormBuilder, private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void { }
 
-  // loginProcessHere() {
-  //   const data = this.fbFormGroup.value;
-  //   console.log(data);
-
-  //   if (data.username === 'angular' && data.password === 'admin') {
-  //     sessionStorage.setItem('sid', 'true');
-  //     this.router.navigate(['home/home-page']);
-  //   } else {
-  //     this.uiInvalidCredential = true;
-  //   }
-  // }
-
-  // Using DB validation
   async loginProcessHere() {
-    const data = this.fbFormGroup.value;
-    console.log(data);
-    const url = "http://localhost:3000/login";
-    const result: any = await this.http.post(url, data).toPromise();
+    const input = this.myFormGroup.value;
+    console.log(input);
+    const url = "http://localhost:3000/user";
+    const result: any = await this.http.post(url, input).toPromise();
+
     if (result.opr) {
       sessionStorage.setItem('sid', 'true');
       this.router.navigate(['home']);
